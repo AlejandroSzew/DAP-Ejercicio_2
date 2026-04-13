46ad7f595563d38323f47704f66756ea2d86b226
AlejandroSzew <49006858@est.ort.edu.ar>
2026-04-13 19:12:03 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp( MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+  
+}
+
+
+
+class _MyAppState extends State<MyApp> {
+  String title = "Login" ;
+  TextEditingController inputTextController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+    home: Scaffold ( 
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(title,style: TextStyle(fontSize: 24),),
+        SizedBox(height: 20,), 
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          controller: inputTextController,
+          keyboardType: TextInputType.datetime,
+          decoration: InputDecoration(
+            hintText: "Escribe aqui",
+            border: OutlineInputBorder(),
+          ),
+        )
         ),
-      ),
-    );
+        SizedBox(height: 20,),
+        ElevatedButton(onPressed: () {
+          String inputText = inputTextController.text;
+          if(inputText == 'Hola'){
+            title = 'Hello';
+
+          }else{
+            title = inputText;
+          }
+          title = inputText;
+
+          setState((){});
+        }
+        
+        , child: Text ("Aceptar"))
+
+      ],)  
+    )
+
+      );
+      
   }
 }
+
